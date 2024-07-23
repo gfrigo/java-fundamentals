62609fcd85fbad551758c6b665dfee1c21588a99
Gabriel Frigo <gfrigo.sena@gmail.com>
2024-07-23 19:02:49 -0300

    variaveis

---
+++ Variaveis.java
@@ -0,0 +1,20 @@
+public class Variaveis {
+
+  // Números inteiros
+	public byte tipoByte = 127; // 8 bits
+	public short tipoShort = 32767; // 16 bits
+	public int tipoInt = 2147483647; // 32 bits
+	public long tipoLong = 9223372036854775807L; // 64 bits
+	
+	// Números fracionários
+	public float tipoFloat = 2.890f; /*32 bits*/
+	public double tipoDouble = 3.40282347000000; /*64 bits*/
+	
+	// Booleanos
+	public boolean tipoBoolean = false;
+	
+	// Texto
+	public String tipoTexto = "Hello World";
+	public char tipoChar = 'G';
+
+}
